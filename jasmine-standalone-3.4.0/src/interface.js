c97e862d1053249f7e4450addfc2bf596977f074
Student <student@MAKERSs-MacBook-Pro-36.local>
2019-06-18 11:04:23 +0100

    API note for future ref

--- jasmine-standalone-3.4.0/src/interface.js
+++ jasmine-standalone-3.4.0/src/interface.js
@@ -34,10 +34,10 @@ $( document ).ready(function() {
   });
 
 
-  // console
-$.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-  console.log(data.main.temp);
-})
+  // API note-
+// $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+//   console.log(data.main.temp);
+// })
 
 
 
